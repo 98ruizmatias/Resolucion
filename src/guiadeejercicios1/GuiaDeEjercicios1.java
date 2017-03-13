d595524fe5a873735c69a596ef8d8892a103ed1e
Matias Ruiz <matias.alejandro.ruiz@hotmail.com>
2017-03-13 16:30:25 -0300

    Primer Ejercicio

--- src/guiadeejercicios1/GuiaDeEjercicios1.java
+++ src/guiadeejercicios1/GuiaDeEjercicios1.java
@@ -1,21 +1,35 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package guiadeejercicios1;
+import java.util.Scanner;
 
 /**
  *
  * @author pwnd
  */
 public class GuiaDeEjercicios1 {
-
-    /**
-     * @param args the command line arguments
-     */
+    
+    /*Ejercicio 1*/
+    
+    public static int minimo(int [] array1){
+    int aux = array1[0];
+    for(int index = 0; index < array1.length; index++){
+        if(array1[index] == aux){
+             aux = array1[index];
+            }
+        }
+    return aux;
+    }
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner in = new Scanner(System.in);
+        System.out.print("Cuantos valores desea ingresar: ");
+        int vector1Tam = in.nextInt();
+        int [] vector1 = {0,0};
+        for(int index1 = 0; index1 < vector1Tam; index1++){
+            System.out.println("Ingrese valor: ");
+            vector1[index1] = in.nextInt();
+        }
+        System.out.print("El minimo de los valores ingresados es: ");
+        System.out.print( GuiaDeEjercicios1.minimo(vector1) );
+        
     }
     
 }
